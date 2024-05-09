817eef0133743b70e8060fb343f67badafde64e5
Adam Joseph Eisen <eisenaj@openmind7.MIT.EDU>
2024-05-09 13:28:43 -0400

    Figure formatting updates

--- sci_style.py
+++ sci_style.py
@@ -1,30 +1,42 @@
 # Figure properties
-figure.figsize: 10,5
+figure.figsize: 8, 6
+figure.dpi: 300
 # Font properties
-font.family: DejaVu Sans
-font.size: 20
+font.family: sans-serif
+font.sans-serif: Arial
+font.size: 6
+axes.labelsize: 6.5   # Specify the font size for xlabel and ylabel
+axes.titlesize: 8   # Specify the font size for the title
+legend.fontsize: 6
 # Axes properties
-axes.linewidth: 2
-axes.labelpad: 10
+axes.linewidth: 0.5
+axes.labelpad: 6
 axes.prop_cycle: cycler(color=['008fd5', 'fc4f30', 'e5ae38', '6d904f', '8b8b8b', '810f7c'])
-axes.titlepad: 15
+axes.titlepad: 8
 # Tick properties
 # x-axis
-xtick.top: True
+xtick.top: False
+axes.spines.top: False
 xtick.direction: in
-xtick.major.size: 7
-xtick.major.width: 2
-xtick.minor.size: 5
-xtick.minor.width: 2
+xtick.major.size: 2
+xtick.major.width: 0.5
+xtick.minor.size: 1
+xtick.minor.width: 0.5
+xtick.labelsize: 6
 # y-axis
-ytick.right: True
+ytick.right: False
+axes.spines.right: False
 ytick.direction: in
-ytick.major.size: 7
-ytick.major.width: 2
-ytick.minor.size: 5
-ytick.minor.width: 2
+ytick.major.size: 2
+ytick.major.width: 0.5
+ytick.minor.size: 1
+ytick.minor.width: 0.5
+ytick.labelsize: 6
 # Line properties
-lines.linewidth: 2
+lines.linewidth: 0.5
+lines.markersize: 0.005
 # Legend properties
 legend.framealpha: 1
 legend.frameon: False
+# Scatter properties
+scatter.marker : o  # Use circles as markers
